716c799cdbc4acc7b77edb2f34ba29d404706232
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-10-23 11:49:48 +0200

    implement movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header"
+import MovieList from "./components/MovieList";
+
 function App() {
   return (
     <>
-      <h1>Welcome to Popcorn Time!</h1>
+      <Header />
+      <MovieList />
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
